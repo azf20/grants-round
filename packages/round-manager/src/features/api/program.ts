1b4b019bf6dff0a115dabecb5e07ce0c61008df1
Shavina Chau <shavina.chau@gmail.com>
2022-09-06 17:50:35 -0500

    chore: add getProgramById api

--- packages/round-manager/src/features/api/program.ts
+++ packages/round-manager/src/features/api/program.ts
@@ -1,14 +1,13 @@
 import { fetchFromIPFS, graphql_fetch } from "./utils"
 import { Program } from "./types"
 
-// TODO consider always returning an array and also error state, so return type is consistent
 /**
  * Fetch a list of programs
  * @param address - a valid program operator
  * @param signerOrProvider - signer
  *
  */
-export async function listPrograms (address: string, signerOrProvider: any): Promise<Program[]> {
+export async function listPrograms(address: string, signerOrProvider: any): Promise<Program[]> {
   try {
     // fetch chain id
     const { chainId } = await signerOrProvider.getNetwork()
@@ -58,5 +57,51 @@ export async function listPrograms (address: string, signerOrProvider: any): Pro
   } catch (err) {
     console.log("error", err)
     throw Error("Unable to fetch programs")
+  }
+}
+
+export async function getProgramById(programId: string, signerOrProvider: any): Promise<Program> {
+  try {
+    // fetch chain id
+    const { chainId } = await signerOrProvider.getNetwork()
+
+    // get the subgraph for program by $programId
+    const res = await graphql_fetch(
+      `
+              query GetPrograms($programId: String) {
+                programs(where: {
+                  id: $programId
+                }) {
+                  id
+                  metaPtr {
+                    protocol
+                    pointer
+                  }
+                  roles(where: {
+                    role: "0xaa630204f2780b6f080cc77cc0e9c0a5c21e92eb0c6771e709255dd27d6de132"
+                  }) {
+                    accounts {
+                      address
+                    }
+                  }
+                }
+              }
+            `,
+      chainId,
+      {programId}
+    )
+
+    const programDataFromGraph = res.data.programs[0]
+    const metadata = await fetchFromIPFS(programDataFromGraph.metaPtr.pointer)
+
+    return {
+      id: programDataFromGraph.id,
+      metadata,
+      operatorWallets: programDataFromGraph.roles[0].accounts.map((program: any) => program.address)
+    }
+
+  } catch (err) {
+    console.log("error", err)
+    throw Error("Unable to fetch program")
   }
 }
